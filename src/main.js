a3b135ee5a3fdd42284b78d51227e39162f60771
Thiago Hofmeister <thiago.souza@moovin.com.br>
2018-02-23 10:41:54 -0300

    Alterações nas portas

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import store from './store'
 import router from './router'
-import Icon from 'vue-awesome/icons'
+// import Icon from 'vue-awesome/icons'
 
 Vue.config.productionTip = false
 
-Vue.component(Icon)
+// Vue.component(Icon)
 
 require('./styles/custom-bootstrap.scss')
 require('../node_modules/bootstrap/scss/bootstrap.scss')
