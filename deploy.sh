ae616f337d1e245e31c8963bb7ab266d14868b6d
Thiago Hofmeister <thiago.hofmeister@gmail.com>
2018-04-19 23:05:22 -0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e # termina o script com um código diferente de 0 se alguma coisa falhar
+
+# roda o script de build da nossa aplicação
+npm run build
+
+# pull requests e commits para outras branches diferentes da master 
+# não devem fazer o deploy, isso é opcional caso queira deletar as próximas 6 linhas
+# fique a vontade
+SOURCE_BRANCH="master"
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy."
+    exit 0
+fi
